f49e52c8ac9d7dd2387bfa3242533389c1eac0ba
xpillons <xpillons@SURFACEBOOK-XPS>
2017-07-03 15:23:20 +0200

    fix Autoscaling.Function

--- Pinta.AutoScaling.Functions/Properties/AssemblyInfo.cs
+++ Pinta.AutoScaling.Functions/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c9e4a45-000f-436b-ba21-b2a7a087d0e2")]
+[assembly: Guid("5f5bf11c-95ae-458f-aaa5-d082c10128e7")]
 
 // Version information for an assembly consists of the following four values:
 //
